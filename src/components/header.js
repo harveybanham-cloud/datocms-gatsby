61a6dccfb773036efa901f3bdc5844dc90bf3d34
Harvey Banham <harvey.banham@flag.co.uk>
2020-04-28 15:08:12 +0100

    Swap StaticQuery with useStaticQuery

--- src/components/header.js
+++ src/components/header.js
@@ -1,44 +1,44 @@
-import { Link, StaticQuery, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import React from "react"
 
-const Header = () => (
-  <StaticQuery
-  query={graphql`
+const Header = () => {
+  const data = useStaticQuery(graphql`
       query GlobalHeadingQuery {
         datoCmsGlobal {
           globalHeading
         }
       }`
-    }
-  render={data => (
-      <header
+  )
+
+
+  return (
+    <header
+      style={{
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
+      }}
+    >
+      <div
         style={{
-          background: `rebeccapurple`,
-          marginBottom: `1.45rem`,
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
         }}
       >
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `1.45rem 1.0875rem`,
-          }}
-        >
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`,
-              }}
-            >
-              {data.datoCmsGlobal.globalHeading}
-            </Link>
-          </h1>
-        </div>
-      </header>
-  )}
-  />
-)
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+          {data.datoCmsGlobal.globalHeading}
+          </Link>
+        </h1>
+      </div>
+    </header>
+  )
+}
 
 export default Header
